165912cc973b08ccaecf524e378f32d3a136f87a
vishal-vignesh14 <vishalvignesh65@gmail.com>
2025-04-09 01:13:27 +0530

    changes done

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 // Cloudinary config
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
+  cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
